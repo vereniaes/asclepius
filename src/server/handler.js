9e8adfa26e8c7bcdb494befb422b6cb52577d952
Titasari Pratiwi <146017679+vereniaes@users.noreply.github.com>
2024-12-04 13:06:03 +0700

    Update handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -8,7 +8,7 @@ async function postPredictHandler(request, h) {
   const { image } = request.payload;
   const { model } = request.server.app;
 
-  const { result, suggestion } = await predictClassification(model, image);
+  const { confidenceScore, label, suggestion } = await predictClassification(model, image);
   const id = crypto.randomUUID();
   const createdAt = new Date().toISOString();
 
